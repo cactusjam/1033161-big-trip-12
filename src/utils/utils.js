964a0d8e410aebf63dc8087f68ab0344140abfd4
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-08-27 23:47:54 +0300

    удалены закоммент части кода

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,46 +6,6 @@ const getTypeParticle = (type) => ACTIVITY_TYPES.includes(type)
 
 const getFirstUpperCase = (element) => element[0].toUpperCase() + element.slice(1);
 
-// const sortCardsByTimeDown = (pointA, pointB) => pointB.startDate - pointA.startDate;
-
-// const sortCardsByPriceDown = (pointA, pointB) => pointB.price - pointA.price;
-
-// const getWeightForNullDate = (dateA, dateB) => {
-//   if (dateA === null && dateB === null) {
-//     return 0;
-//   }
-
-//   if (dateA === null) {
-//     return 1;
-//   }
-
-//   if (dateB === null) {
-//     return -1;
-//   }
-
-//   return null;
-// };
-
-// const sortUp = (pointA, pointB) => {
-//   const weight = getWeightForNullDate(pointA.startDate, pointB.startDate);
-
-//   if (weight !== null) {
-//     return weight;
-//   }
-
-//   return pointA.startDate.getTime() - pointB.startDate.getTime();
-// };
-
-// const sortDown = (pointA, pointB) => {
-//   const weight = getWeightForNullDate(pointA.startDate, pointB.startDate);
-
-//   if (weight !== null) {
-//     return weight;
-//   }
-
-//   return pointB.startDate.getTime() - pointA.startDate.getTime();
-// };
-
 export const sortEventsByPrice = (priceA, priceB) => {
   return priceB.price - priceA.price;
 };
